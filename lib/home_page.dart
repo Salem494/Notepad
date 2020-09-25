4912400f8cf0ba0db7bbf1edb5e89f9bf4315b2f
salem807 <salemmahrous807@gmail.com>
2020-09-25 23:26:28 +0200

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,182 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'database.dart';
+import 'edit_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Database _database;
+
+//databaseFromJson method
+  Database databaseFromJson(String str) {
+    final dataFromJson = json.decode(str);
+    return Database.fromJson(dataFromJson);
+  }
+
+  //databaseToJson method
+  String databaseToJson(Database data) {
+    final dataToJson = data.toJson();
+    return json.encode(dataToJson);
+  }
+
+  //_loadJournals method
+  Future<List<Journal>> _loadJournals() async {
+    await DatabaseFileRoutines().readJournals().then((journalsJson) {
+      _database = databaseFromJson(journalsJson);
+      _database.journal
+          .sort((comp1, comp2) => comp2.date.compareTo(comp1.date));
+    });
+    return _database.journal;
+  }
+
+//  _addOrEditJournal Method
+  void _addOrEditJournal({bool add, int index, Journal journal}) async {
+    JournalEdit _journalEdit = JournalEdit(action: '', journal: journal);
+    _journalEdit = await Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => EditNote(
+                add: add,
+                index: index,
+                journalEdit: _journalEdit,
+              ),
+          fullscreenDialog: true),
+    );
+    switch (_journalEdit.action) {
+      case 'Save':
+        if (add) {
+          setState(() {
+            _database.journal.add(_journalEdit.journal);
+          });
+        } else {
+          setState(() {
+            _database.journal[index] = _journalEdit.journal;
+          });
+        }
+        DatabaseFileRoutines().writeJournals(databaseToJson(_database));
+        break;
+      case 'Cancel':
+        break;
+      default:
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My All Note'),
+        centerTitle: true,
+        elevation: 7,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 15.0),
+        child: FutureBuilder(
+            initialData: [],
+            future: _loadJournals(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              return !snapshot.hasData
+                  ? Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : _buildListViewSeparated(snapshot);
+            }),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Padding(
+          padding: EdgeInsets.all(24.0),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _addOrEditJournal(add: true, index: -1, journal: Journal());
+        },
+        child: Icon(Icons.add),
+        tooltip: 'Add New Note',
+      ),
+    );
+  }
+
+  //_buildListViewSeparated widget
+  Widget _buildListViewSeparated(AsyncSnapshot snapshot) {
+    return ListView.separated(
+      itemCount: snapshot.data.length,
+      itemBuilder: (BuildContext context, int index) {
+        String _titleDate = DateFormat.yMMMd()
+            .format(DateTime.parse(snapshot.data[index].date));
+        String _subtitle =
+            snapshot.data[index].mood + "\n" + snapshot.data[index].note;
+        return Dismissible(
+          key: Key(snapshot.data[index].id),
+          background: Container(
+            color: Colors.red,
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(left: 16.0),
+            child: Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+          ),
+          secondaryBackground: Container(
+            color: Colors.red,
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.only(right: 16.0),
+            child: Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+          ),
+          child: ListTile(
+            leading: Column(
+              children: <Widget>[
+                Text(
+                  DateFormat.d()
+                      .format(DateTime.parse(snapshot.data[index].date)),
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25.0,
+                      color: Colors.blue),
+                ),
+                Text(DateFormat.E()
+                    .format(DateTime.parse(snapshot.data[index].date))),
+              ],
+            ),
+            title: Text(
+              _titleDate,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(_subtitle),
+            onTap: () {
+              _addOrEditJournal(
+                add: false,
+                index: index,
+                journal: snapshot.data[index],
+              );
+            },
+          ),
+          onDismissed: (direction) {
+            setState(() {
+              _database.journal.removeAt(index);
+            });
+            DatabaseFileRoutines().writeJournals(databaseToJson(_database));
+          },
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return Divider(
+          color: Colors.grey,
+        );
+      },
+    );
+  }
+}
